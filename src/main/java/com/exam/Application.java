10c977e898ea5e45d8e213a88d68f2dc88d0741a
dlduddms08 <dlduddms08@icloud.com>
2024-07-09 19:16:26 +0900

    develop test

--- src/main/java/com/exam/Application.java
+++ src/main/java/com/exam/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//test_young
+//test_1
 @SpringBootApplication
 public class Application {
 
